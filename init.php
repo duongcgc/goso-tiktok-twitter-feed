817ec34cb07f708cb2f02fe98d89c96a6c8dd8d2
John Nguyen <duongcgc@gmail.com>
2022-07-24 15:12:30 +0700

    Init

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Goso Social Feed
+Plugin URI: http://gosodesign.com/
+Description: A plugin to help you connect to some socials media ( like Twitter ) to show the feed of those socials media.
+Version: 1.1
+Author: GosoDesign
+Author URI: http://themeforest.net/user/gosodesign?ref=gosodesign
+*/
+
+define( 'PENCI_SOLEDAD_SOCIAL_FEED', '1.1' );
+require_once dirname( __FILE__ ) . '/lib/twitter.php';
